dad5200643212c37795786f3fd6a5f344e5153a0
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-06-16 16:58:59 -0700

    added another router  to my deliveryRouter

--- router/deliveryRouter.ts
+++ router/deliveryRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createDelivery,
   deleteDelivery,
+  viewAllDelivery,
 } from "../controller/deliveryController";
 
 const routerDelivery: Router = Router();
@@ -10,5 +11,6 @@ routerDelivery.route("/create-delivery/:adminID").post(createDelivery);
 routerDelivery
   .route("/delete-delivery/:adminID/:deliveryID")
   .delete(deleteDelivery);
+routerDelivery.route("/view-all-delivery").delete(viewAllDelivery);
 
 export default routerDelivery;
