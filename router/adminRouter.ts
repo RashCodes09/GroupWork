92d2cf0db2ccdc86d1a8458544a798bd37c62b84
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-06-16 16:02:28 -0700

    added another router to my adminRouter

--- router/adminRouter.ts
+++ router/adminRouter.ts
@@ -10,5 +10,6 @@ const router = Router();
 router.route("/create-admin").post(createAdmin);
 router.route("/verify-admin/:adminID").patch(verifyAdmin);
 router.route("/sign-admin").post(signInAdmin);
+
 router.route("/get-admin/:adminID").get(getAdmin);
 export default router;
