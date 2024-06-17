a1a48c8e4483c3639df94e1636cfad6cc893fa72
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-06-16 17:54:14 -0700

    pushim mainError file from error folder

---
+++ error/mainError.ts
@@ -0,0 +1,23 @@
+import { iError } from "../utlis/interface";
+import { HTTP } from "../utlis/enum";
+
+export class mainError extends Error {
+  public readonly name: string;
+  public readonly message: string;
+  public readonly status: HTTP;
+  public readonly success: boolean = false;
+
+  constructor(args: iError) {
+    super(args.message);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    this.name = args.name;
+    this.message = args.message;
+    this.status = args.status;
+
+    if (this.success !== undefined) this.success = args.success;
+
+    Error.captureStackTrace;
+  }
+}
